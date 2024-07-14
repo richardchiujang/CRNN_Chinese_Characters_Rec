c1f2528fb6b26a3a7b16ba8db58552cae992f5a6
Richard Chiu <rchiu3210@gmail.com>
2024-07-14 14:01:28 +0800

    mod run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ call C:\anaconda3\Scripts\activate.bat C:\anaconda3
 call conda activate trainocr
 cd C:\develop\CRNN_Chinese_Characters_Rec
 python train.py
-python train.py
+@REM python train.py
 @REM python train.py
 @REM python train.py
 @REM python train.py
